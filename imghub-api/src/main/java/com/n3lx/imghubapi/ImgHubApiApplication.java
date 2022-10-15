9e3aeb355d339db9b7c246348ff34a8b02c352ff
Wojtek Hołowiak <w.holowiak@gmail.com>
2022-10-15 19:20:51 +0200

    Created and truncated an empty project with help of Spring Initializr.

---
+++ imghub-api/src/main/java/com/n3lx/imghubapi/ImgHubApiApplication.java
@@ -0,0 +1,13 @@
+package com.n3lx.imghubapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgHubApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImgHubApiApplication.class, args);
+    }
+
+}
---
+++ imghub-api/src/test/java/com/n3lx/imghubapi/ImgHubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.n3lx.imghubapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImgHubApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
