b65c4d24e331ce27926e55d7b97da941b75ba4d5
Wojtek Hołowiak <w.holowiak@gmail.com>
2022-10-16 11:20:00 +0200

    Created JPA entities and added an integration test.

---
+++ imghub-api/src/main/java/com/n3lx/imghubapi/entity/Resource.java
@@ -0,0 +1,53 @@
+package com.n3lx.imghubapi.entity;
+
+import javax.persistence.*;
+import java.time.ZonedDateTime;
+import java.util.TimeZone;
+
+@Entity
+@Table(name = "resources")
+public class Resource {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private ZonedDateTime uploadTime;
+
+    private TimeZone uploadTimeZone;
+
+    private String resourceName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ZonedDateTime getUploadTime() {
+        return uploadTime;
+    }
+
+    public void setUploadTime(ZonedDateTime uploadTime) {
+        this.uploadTime = uploadTime;
+    }
+
+    public TimeZone getUploadTimeZone() {
+        return uploadTimeZone;
+    }
+
+    public void setUploadTimeZone(TimeZone uploadTimeZone) {
+        this.uploadTimeZone = uploadTimeZone;
+    }
+
+    public String getResourceName() {
+        return resourceName;
+    }
+
+    public void setResourceName(String resourceName) {
+        this.resourceName = resourceName;
+    }
+
+}
